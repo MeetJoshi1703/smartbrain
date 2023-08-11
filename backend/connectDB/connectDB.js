91d32f926c27f0256ccd8e473c97ba236c1a6e5c
MeetJoshi1703 <98758359+MeetJoshi1703@users.noreply.github.com>
2023-08-11 13:07:04 +0530

    updating folder structure

--- backend/connectDB/connectDB.js
+++ backend/connectDB/connectDB.js
@@ -1,10 +1,12 @@
+require('dotenv').config('../.env')
+
 const knex = require('knex');
 const db = knex({
     client:'pg',
     connection:{
         host:"127.0.0.1",
         user:'postgres',
-        password:'1703',
+        password:process.env.PASSWORD,
         database:'smartbrain'
     }
 })
